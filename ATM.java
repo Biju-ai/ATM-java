4789992270233d4c87be1c489fc405c91050426b
Biju-ai <bijushrestha768@gmail.com>
2025-05-05 14:37:15 +0545

    ATM in java

---
+++ ATM.java
@@ -0,0 +1,84 @@
+
+import java.util.*;
+public class ATM {
+    public static void main(String[] args) {
+
+        int acc=1;
+        int pin=1;
+        double balance=0;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Wellcome to ATM");
+        System.out.print("Enter your account number :");
+        int account=s.nextInt();
+        System.out.print("Enter the pin :");
+        int pass=s.nextInt();
+        if((acc==account) && (pin==pass)){
+            while (true) {
+
+                System.out.println("\nSelect the number you want to access");
+
+                System.out.println("Type 1 = View available amount");
+                System.out.println("Type 2 = withdraw amount");
+                System.out.println("Type 3 = Deposite ");
+                System.out.println("type 4 = Exit ");
+                System.out.print("\nChoice :");
+                int choose = s.nextInt();
+
+                if (choose == 1) {
+                    System.out.println("your amount is" + " " + "Rs" + " " + balance);
+
+                } else if (choose == 2) {
+                    System.out.print("Enter the amount you want to withdrow:");
+                    double withdraw = s.nextInt();
+
+                    if (withdraw > balance) {
+                        System.out.println("Insufficent amount!");
+
+                    } else if (withdraw <= 0) {
+                        System.out.println("Sorry plis enter the positive amount");
+                    } else if (withdraw <= 500) {
+                        System.out.println("Sorry You can't withdraw less then Rs 500");
+                    } else {
+                        balance = balance - withdraw;
+                        System.out.println("Withdraw amount successfully\nYour current amount is " + " " + "Rs" + " " + balance);
+                    }
+
+                } else if (choose == 3) {
+                    System.out.println("How much money you want to deposite ?");
+                    double deposite = s.nextInt();
+
+                    if (deposite >= 200000) {
+                        System.out.println("Sorry you can't deposite more then 2 laks once at a time");
+
+                    } else if (deposite <= 0) {
+                        System.out.println("Please enter the positive value");
+
+                    } else {
+                        balance = balance + deposite;
+                        System.out.println("Add balance successfully\nYour current amount is" + " " + "Rs" + " " + balance);
+                        System.out.println();
+                    }
+
+                } else if (choose == 4) {
+                    System.out.println("\n**");
+                    System.out.println("  Please collect your card");
+                    System.out.println("  Thank you for visiting our ATM");
+                    System.out.println("\n");
+
+
+
+                } else {
+                    System.out.println("Please enter the correct number, Thankyou");
+                    System.exit(0);
+                }
+            }
+        }
+        else{
+            System.out.println("Sorry the pin is wrong,Try again later" );
+}
+    }
+}
+
+
+
+
